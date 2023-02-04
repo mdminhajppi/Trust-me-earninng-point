d43fec8f6e4b6eed7cc111fb9e00457942c9458f
mdminhajppi <109678561+mdminhajppi@users.noreply.github.com>
2023-02-04 14:44:37 +0600

    Add files via upload

---
+++ home.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Trust Me Earning Point</title>
+   <link rel="icon" href="print-204671996.jpg" type="image/gif/jpg">
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+   <a href="home.php" class="logo"><h1>$ Earn</h1></a>
+
+   <nav class="navbar">
+      <a href="home.php"target="_blank">Home</a>
+      <a href="https://www.facebook.com/profile.php?id=100090292230201&mibextid=ZbWKwL"target="_blank">Contact</a>
+      <a href="https://l.facebook.com/l.php?u=https%3A%2F%2Ft.me%2F%2BmK0JIFeGaVhlNWY1%3Ffbclid%3DIwAR1MPXaN3JWx_F8QripZWgAvnaHrRVcyOXapvjZONESsvAhWc6sh7AYLm1Y&h=AT0pINzBdI1fufZX7RJDEMDyAdEVxNHtpRBPXyI5vxwaDmR9eRKQq4ovdmCakXam0ATikfNHQ4TyP0PJ4ZxqYzZ7k9O-byDfilp4ijiseSQ3WceVKg8xNpA6aA2KyFRCbO9Ivg"target="_blank">Telegram</a>
+      <a href="https://l.facebook.com/l.php?u=https%3A%2F%2Ftumi-j-amar.netlify.app%2F%3Ffbclid%3DIwAR0VV5f13Z16_Ndf0ThBPKfCoYc2B_z6yxGv5g-znfDQWwSc92a3anJV8kA&h=AT0rcxnRWoq2FxtwCPbferr4OAMzUQKHAHHBEy4rZx-IhMjJTqV4Phe-S6KxAWcg4AnzU_unuFxO3MVNQk93Cxey0HhWukP4meFPp6otVuS-f7yqjySzYKf52cq3EOjvsSIVXg"target="_blank">$Earn</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home">
+
+   <div class="swiper home-slider">
+
+      <div class="swiper-wrapper">
+
+         <div class="swiper-slide slide" style="background:url(images/sadik.jpg) no-repeat">
+            <div class="content">
+               <span>Money is Everythikg In The World</span>
+               <h3>TRUST ME EARNING POINT</h3>
+               <a href="package.php" class="btn">discover more</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide" style="background:url(images/minhaj0.jpg) no-repeat">
+            <div class="content">
+               <span>Money is Everythikg In The World</span>
+               <h3>discover the new places @Minhaj $ @Niyamot</h3>
+               <a href="package.php" class="btn">discover more</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide" style="background:url(images/minhaj4.jpg) no-repeat">
+            <div class="content">
+               <span>Money is Everythikg In The World</span>
+               <h3>TRUST ME EARNING POINT</h3>
+               <a href="package.php" class="btn">discover more</a>
+            </div>
+         </div>
+         
+      </div>
+
+      <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>
+
+   </div>
+
+</section>
+
+<!-- home section ends -->
+
+<!-- services section starts  -->
+
+<section class="services">
+
+   <h1 class="heading-title"> our services </h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/sadik1.png" alt="">
+         <h3>adventure</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/sadik2.png" alt="">
+         <h3>tour guide</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/sadik3.png" alt="">
+         <h3>trekking</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/sadik4.png" alt="">
+         <h3>camp fire</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/sadik5.png" alt="">
+         <h3>off road</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/sadik6.png" alt="">
+         <h3>camping</h3>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- services section ends -->
+
+<!-- home about section starts  -->
+
+<section class="home-about">
+
+   <div class="image">
+      <img src="images/about 1.jpg" alt="">
+   </div>
+
+   <div class="content">
+      <h3>about us</h3>
+      <p>Amra amader moto kore sobay k reffer kori and sobay mile bissaser sathe kaj kori.  taka incom kori & sobar majhe Njeke potisthito kore gore tuli ,Amra sob somoy apnader sathe jukto thakbo ..inshalla sobay doya korben jeno oneker kormo khetro korte pari ..</p>
+      <a href="about.php" class="btn">read more</a>
+   </div>
+
+</section>
+
+<!-- home about section ends -->
+
+<!-- home packages section starts  -->
+
+<section class="home-packages">
+
+   <h1 class="heading-title"> our packages </h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="images/ime.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Everyone Refer And Earn Money</h3>
+            <p>We Refer everyone, We Earn 20 rupees by referring one person.!</p>
+            <a href="book.php" class="btn">Hurry up</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/images1.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Everyone join Telegram..></h3>
+            <p>We Refer everyone, We Earn 20 rupees by referring one person.!</p>
+            <a href="https://l.facebook.com/l.php?u=https%3A%2F%2Ft.me%2F%2BmK0JIFeGaVhlNWY1%3Ffbclid%3DIwAR1MPXaN3JWx_F8QripZWgAvnaHrRVcyOXapvjZONESsvAhWc6sh7AYLm1Y&h=AT0pINzBdI1fufZX7RJDEMDyAdEVxNHtpRBPXyI5vxwaDmR9eRKQq4ovdmCakXam0ATikfNHQ4TyP0PJ4ZxqYzZ7k9O-byDfilp4ijiseSQ3WceVKg8xNpA6aA2KyFRCbO9Ivg"target="_blank"class="btn"> join Now</a>
+         </div>
+      </div>
+      
+      <div class="box">
+         <div class="image">
+            <img src="images/images.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Everyone join Youtube..></h3>
+            <p>We Refer everyone, We Earn 20 rupees by referring one person.!</p>
+            <a href="https://l.facebook.com/l.php?u=https%3A%2F%2Fyoutub.com%2F%40trustmeearningpoint%3Ffbclid%3DIwAR1om0nSH0-sBwwNO3ByZWHauY8jHB1hv2VvIAmEPP5CJA2B3jecgVMYL2w&h=AT10vabolv655ZCnJIEhF_sqButQXtX5vSCV5Al-tslHz3f9ev5XRKGs_fGdurzmlWjvT8lnOauYntl-XG54ZNe9iV-nDVGIsiDrol7Won1ykvuAEwb_AeQZyXQ4GgUT13L4kw" class="btn">Wach Now</a>
+         </div>
+      </div>
+
+   </div>
+
+   <div class="load-more"> <a href="package.php" class="btn">load more</a> </div>
+
+</section>
+
+<!-- home packages section ends -->
+
+<!-- home offer section starts  -->
+
+<section class="home-offer">
+   <div class="content">
+      <h3>Bouns 50% on</h3>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure tempora assumenda, debitis aliquid nesciunt maiores quas! Magni cumque quaerat saepe!</p>
+      <a href="book.php" class="btn">Earn now</a>
+   </div>
+</section>
+
+<!-- home offer section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="https://www.facebook.com/profile.php?id=100090292230201&mibextid=ZbWKwL"target="_blank"> <i class="fas fa-angle-right"></i> Contact</a>
+         <a href="https://l.facebook.com/l.php?u=https%3A%2F%2Ft.me%2F%2BmK0JIFeGaVhlNWY1%3Ffbclid%3DIwAR1MPXaN3JWx_F8QripZWgAvnaHrRVcyOXapvjZONESsvAhWc6sh7AYLm1Y&h=AT0pINzBdI1fufZX7RJDEMDyAdEVxNHtpRBPXyI5vxwaDmR9eRKQq4ovdmCakXam0ATikfNHQ4TyP0PJ4ZxqYzZ7k9O-byDfilp4ijiseSQ3WceVKg8xNpA6aA2KyFRCbO9Ivg"target="_blank"> <i class="fas fa-angle-right"></i> telegram</a>
+         <a href="https://l.facebook.com/l.php?u=https%3A%2F%2Ftumi-j-amar.netlify.app%2F%3Ffbclid%3DIwAR0VV5f13Z16_Ndf0ThBPKfCoYc2B_z6yxGv5g-znfDQWwSc92a3anJV8kA&h=AT0rcxnRWoq2FxtwCPbferr4OAMzUQKHAHHBEy4rZx-IhMjJTqV4Phe-S6KxAWcg4AnzU_unuFxO3MVNQk93Cxey0HhWukP4meFPp6otVuS-f7yqjySzYKf52cq3EOjvsSIVXg"target="_blank"> <i class="fas fa-angle-right"></i> $ Earn</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i><img src="images/rock.jpg" alt="">
+         send Money </a>
+         <a href="#"> <i class="fas fa-phone"></i>*(017208101616).<brk>*(013070452857). </a>
+         <a href="#"> <i class="fas fa-envelope"></i> mdminhajppi8@gmail.com </a>
+         <a href="#"> <i class="fas fa-map"></i>Dhaka, Bangladesh - 1234 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> lraminkedin </a>
+      </div>
+
+   </div>
+
+   <div class="credit"> created by <span>mr. web designer</span> | all rights reserved! </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
